cc26291081f596ccf6b89fa5e388081f019d8593
Juneh19 <88454677+Juneh19@users.noreply.github.com>
2021-08-04 16:22:02 -0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,5 +1,5 @@
 nome = input("Digite o seu nome: ")
-dia = int(input("Digite o dia que você nasceu: "))
-mes = int(input("Digite o mês que você nasceu: "))
-ano = int(input("Digite o ano que você nasceu: "))
+dia = input("Digite o dia que você nasceu: ")
+mes = input("Digite o mês que você nasceu: ")
+ano = input("Digite o ano que você nasceu: ")
 print("Seja muito bem vinde {}! Uhu. A data do seu aniversário é {}/{}/{}.".format(nome, dia, mes, ano))
