f62747118eb58cf126f1abd519037bb9ef2a7d2a
Juneh19 <88454677+Juneh19@users.noreply.github.com>
2021-08-04 16:23:23 -0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,4 +1,4 @@
-numero_um = int(input("Digite o primeiro número: "))
-numero_dois = int(input("Digite o segundo número: "))
-soma = int(numero_um + numero_dois)
+numero_um = float(input("Digite o primeiro número: "))
+numero_dois = float(input("Digite o segundo número: "))
+soma = float(numero_um + numero_dois)
 print("A soma dos dois números é: {}".format(soma))
